874cc8acbfed27e8f84c5d051ced644840858f78
aj <anupjena529@gmail.com>
2022-12-01 16:21:27 +0530

    update

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,45 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <div className="container">
-        <Link href="/" passHref>
-          <h2> UI Blog</h2>
-        </Link>
+    <navbar class="navbar-container">
+      <div class="logo-container">
+        <a href="index.html">UI BLOG</a>
       </div>
-    </header>
+
+      <div class="bars">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+
+      <ul class="nav-items">
+        <li class="nav-link">
+          <a href="#">Home</a>
+        </li>
+        <li class="nav-link">
+          <a href="#">Service</a>
+        </li>
+        <li class="nav-link">
+          <a href="#">Projects</a>
+        </li>
+        <li class="nav-link">
+          <a href="#">About</a>
+        </li>
+        <div class="login-register">
+          <a href="#" class="button">
+            Login
+          </a>
+          <a href="#" class="button">
+            Register
+          </a>
+        </div>
+      </ul>
+    </navbar>
   );
 }
+let bar = document.querySelector(".bars"),
+  navItem = document.querySelector(".nav-items");
+
+bar.addEventListener("click", () => {
+  navItem.classList.toggle("active");
+});
--- components/Post.js
+++ components/Post.js
@@ -2,18 +2,20 @@ import Link from "next/link";
 
 export default function Post({ post }) {
   return (
-    <div className="card">
-      <img src={post.frontmatter.cover_image} alt="" />
+    <>
+      <div className="card">
+        <img src={post.frontmatter.cover_image} alt="" />
 
-      <div className="post-date">Posted on {post.frontmatter.date}</div>
+        <div className="post-date">Posted on {post.frontmatter.date}</div>
 
-      <h3>{post.frontmatter.title}</h3>
+        <h3>{post.frontmatter.title}</h3>
 
-      <p>{post.frontmatter.excerpt}</p>
+        <p>{post.frontmatter.excerpt}</p>
 
-      <Link href={`/blog/${post.slug}`}>
-        <p className="btn">Read More</p>
-      </Link>
-    </div>
+        <Link href={`/blog/${post.slug}`}>
+          <p className="btn">Read More</p>
+        </Link>
+      </div>
+    </>
   );
 }
